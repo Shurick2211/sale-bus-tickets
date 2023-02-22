ce315e1abeaf46166b2f6f8c6e11cceab4d15a20
sn <shurick2211@gmail.com>
2023-02-22 19:15:55 +0200

    ticket-environment+

--- src/main/java/com/nimko/salebustikets/ticketmanegment/dto/TicketDto.java
+++ src/main/java/com/nimko/salebustikets/ticketmanegment/dto/TicketDto.java
@@ -1,12 +1,12 @@
 package com.nimko.salebustikets.ticketmanegment.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
+@Schema(name = "Схема покупки ticket", description = "DTO для фронтенда!")
 public class TicketDto {
   private String fullName;
   private String flightId;
--- src/main/java/com/nimko/salebustikets/ticketmanegment/dto/TicketInfoDto.java
+++ src/main/java/com/nimko/salebustikets/ticketmanegment/dto/TicketInfoDto.java
@@ -2,11 +2,13 @@ package com.nimko.salebustikets.ticketmanegment.dto;
 
 import com.nimko.salebustikets.ticketmanegment.models.Flight;
 import com.nimko.salebustikets.utils.PaymentStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Schema(name = "Схема информации про ticket", description = "DTO для фронтенда!")
 public class TicketInfoDto {
   private Flight flight;
   private PaymentStatus paymentStatus;
