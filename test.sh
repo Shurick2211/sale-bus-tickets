132d632ae57df94fd30524d5a134badc4b3a21af
Shurick2211 <shurick2211@gmail.com>
2023-02-24 12:36:00 +0200

    done + port:5000 + swagger-ui.path=/

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -X'POST' 'http://localhost:8080/payments' -H 'Content-Type: application/json' -d '{"full_name": "Василенко Петр Дмитрович", "sum": 15.05}'
+curl -X'POST' 'http://localhost:5000/payments' -H 'Content-Type: application/json' -d '{"full_name": "Василенко Петр Дмитрович", "sum": 15.05}'
 echo -e
 
-curl -X'GET' 'http://localhost:8080/payments?id=1'
+curl -X'GET' 'http://localhost:5000/payments?id=1'
 echo -e
 
-curl -X 'POST' 'http://localhost:8080/tickets' -H 'accept: */*' -H 'Content-Type: application/json'  -d '{"full_name": "Петров Петр Птрович","flight_id": "1A"}'
+curl -X 'POST' 'http://localhost:5000/tickets' -H 'accept: */*' -H 'Content-Type: application/json'  -d '{"full_name": "Петров Петр Птрович","flight_id": "1A"}'
 echo -e
 
-curl -X 'GET' 'http://localhost:8080/tickets/2' -H 'accept: */*'
+curl -X 'GET' 'http://localhost:5000/tickets/2' -H 'accept: */*'
 echo -e
--- test2.sh
+++ test2.sh
@@ -2,7 +2,7 @@
 var1=10
 while [ $var1 -gt 0 ]
 do
-curl -X 'POST' 'http://localhost:8080/tickets' -H 'accept: */*' -H 'Content-Type: application/json'  -d '{"full_name": "Петров Петр Птрович","flight_id": "1A"}'
+curl -X 'POST' 'http://localhost:5000/tickets' -H 'accept: */*' -H 'Content-Type: application/json'  -d '{"full_name": "Петров Петр Птрович","flight_id": "1A"}'
 echo -e
 var1=$[ $var1 - 1 ]
 done
